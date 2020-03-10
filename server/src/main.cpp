74b3ee6e98ec4c7c708474d98baa1643138dceff
Sekanor <sekanor21@gmail.com>
2020-03-10 23:25:06 +0100

    Update main.cpp

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 void testTCPModule() {
     // hc::Server testServer("127.0.0.1", 8100);
     hc::Server dorianServer("", 8080);
-    hc::Server avignon("127.0.0.1", 8088);
-    // hc::Server avignon("127.0.0.1", 11113);
+    // hc::Server avignon("127.0.0.1", 8088);      // TCP-PUSH
+    hc::Server avignon("127.0.0.1", 11113);  // TCP-PULL
     TCPModule tcpModule;
     tcpModule.MakeRequest(nullptr, avignon);
 }
@@ -23,8 +23,8 @@ void testHTTPServer() {
 
 int main(int argc, const char **argv)
 {   
-    testHTTPServer();
-    // testTCPModule();
+    // testHTTPServer();
+    testTCPModule();
 
     return 0;
 }
